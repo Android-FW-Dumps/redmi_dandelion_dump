a46fe838678a9f375f8b4a0810fff5a6dbaad1ec
Sushrut1101 <guptasushrut@gmail.com>
2022-01-17 13:54:07 +0000

    Add extras for dandelion-user 11 RP1A.200720.011 V12.5.4.0.RCDCNXM release-keys
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ join_split_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null >> cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk
+rm -f cust/app/customized/partner-com.autonavi.minimap_12/partner-com.autonavi.minimap_12.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null >> cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk
+rm -f cust/app/customized/partner-com.eg.android.AlipayGphone_23/partner-com.eg.android.AlipayGphone_23.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null >> cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk
+rm -f cust/app/customized/partner-com.baidu.searchbox_9/partner-com.baidu.searchbox_9.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.kuaishou.nebula_152/partner-com.kuaishou.nebula_152.apk.* 2>/dev/null >> cust/app/customized/partner-com.kuaishou.nebula_152/partner-com.kuaishou.nebula_152.apk
+rm -f cust/app/customized/partner-com.kuaishou.nebula_152/partner-com.kuaishou.nebula_152.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null >> cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk
+rm -f cust/app/customized/partner-com.ss.android.ugc.aweme_15/partner-com.ss.android.ugc.aweme_15.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null >> system/system/app/VoiceAssist/VoiceAssist.apk
+rm -f system/system/app/VoiceAssist/VoiceAssist.apk.* 2>/dev/null
+cat system/system/app/Music/Music.apk.* 2>/dev/null >> system/system/app/Music/Music.apk
+rm -f system/system/app/Music/Music.apk.* 2>/dev/null
+cat system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null >> system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk
+rm -f system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
